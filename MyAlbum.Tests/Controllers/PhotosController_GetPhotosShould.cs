65055c9796d7468f9c54d9153efa9bd7b61fac42
NhatTanVu <onchua2006@gmail.com>
2020-01-15 09:37:09 +0800

    - Add reference from Album.Tests to Album.WebSPA - Clean up files

---
+++ MyAlbum.Tests/Controllers/PhotosController_GetPhotosShould.cs
@@ -0,0 +1,18 @@
+using Xunit;
+
+namespace MyAlbum.Tests.Controllers
+{
+    public class PhotosController_GetPhotosShould
+    {
+        public PhotosController_GetPhotosShould()
+        {
+
+        }
+
+        [Fact]
+        public void GetPhotos_ReturnPhotos()
+        {
+            Assert.True(1 == 0);
+        }
+    }
+}
