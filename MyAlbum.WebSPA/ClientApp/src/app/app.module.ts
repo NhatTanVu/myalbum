eab3abf91373197c0ef644b38bc816c319f34be4
NhatTanVu <onchua2006@gmail.com>
2020-05-09 22:49:38 +0800

    - Add loading bar. - Move component styles into corresponding files.

--- MyAlbum.WebSPA/ClientApp/src/app/app.module.ts
+++ MyAlbum.WebSPA/ClientApp/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { LoadingBarHttpClientModule } from '@ngx-loading-bar/http-client';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { HomeComponent } from './components/home/home.component';
-import { CounterComponent } from './components/counter/counter.component';
-import { FetchDataComponent } from './components/fetch-data/fetch-data.component';
 import { PhotoFormComponent } from './components/photo-form/photo-form.component';
 import { PhotoService } from './services/photo.service';
 import { ToastyModule } from 'ng2-toasty';
@@ -21,8 +20,6 @@ import { WindowRef } from './models/WindowRef';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     PhotoFormComponent,
     ViewPhotoComponent
   ],
@@ -32,12 +29,11 @@ import { WindowRef } from './models/WindowRef';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'photos/new', component: PhotoFormComponent },
       { path: 'photos/:id', component: ViewPhotoComponent }
     ]),
-    ToastyModule.forRoot()
+    ToastyModule.forRoot(),
+    LoadingBarHttpClientModule // for HttpClient use
   ],
   providers: [
     PhotoService,
--- MyAlbum.WebSPA/ClientApp/src/app/components/home/home.component.ts
+++ MyAlbum.WebSPA/ClientApp/src/app/components/home/home.component.ts
@@ -6,6 +6,7 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   photos: Photo[];
