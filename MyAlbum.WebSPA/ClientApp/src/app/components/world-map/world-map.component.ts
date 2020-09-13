c58a332317f470f713410750f8b37c6dff0e3f82
NhatTanVu <onchua2006@gmail.com>
2020-09-13 18:40:15 +0800

    Initial commit

---
+++ MyAlbum.WebSPA/ClientApp/src/app/components/world-map/world-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-world-map',
+  templateUrl: './world-map.component.html',
+  styleUrls: ['./world-map.component.css']
+})
+export class WorldMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
