efd8f3af40e760dae3d598fbe662d88c9d08e49e
NhatTanVu <onchua2006@gmail.com>
2020-09-13 20:05:40 +0800

    Initial two-column layout with Google Map

--- MyAlbum.WebSPA/ClientApp/src/app/components/world-map/world-map.component.ts
+++ MyAlbum.WebSPA/ClientApp/src/app/components/world-map/world-map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-world-map',
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorldMapComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('gmap', { static: false }) gmapElement: any;
+  map: google.maps.Map;
+
+  constructor() { 
+    this.initializeMap();
+  }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.initializeMap();
+  }
+
+  initializeMap() {
+    if (this.gmapElement != undefined) {
+      // Create map
+      var mapProp = {
+        center: new google.maps.LatLng(3.140853, 101.693207), // KUL
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+    }
   }
 
 }
