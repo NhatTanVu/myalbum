d987345b4985372a34133a535de74b236c06c40b
NhatTanVu <onchua2006@gmail.com>
2020-10-14 11:37:24 +0800

    - Add zoom effect for images in Explore and World Map. - Immediately update image list after searching World Map.

--- MyAlbum.WebSPA/ClientApp/src/app/components/world-map/world-map.component.ts
+++ MyAlbum.WebSPA/ClientApp/src/app/components/world-map/world-map.component.ts
@@ -13,17 +13,17 @@ export class WorldMapComponent implements OnInit {
   @ViewChild('gmap', { static: false }) gmapElement: any;
   map: google.maps.Map;
   @ViewChild('gmapSearchBox', { static: false }) gmapSearchBox: any;
-  searchBox: google.maps.places.SearchBox;  
+  searchBox: google.maps.places.SearchBox;
   markers: google.maps.Marker[];
   markerCluster: MarkerClusterer;
-  
+
   allPhotos: Photo[];
   viewportPhotos: Photo[];
-  query: any = {};  
+  query: any = {};
 
   constructor(
     private photoService: PhotoService
-  ) { 
+  ) {
     this.initializeMap();
   }
 
@@ -46,7 +46,7 @@ export class WorldMapComponent implements OnInit {
             zoom: 12,
             mapTypeId: google.maps.MapTypeId.ROADMAP
           };
-          this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);          
+          this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
           this.allPhotos = photos;
           if (this.allPhotos.length > 0) {
             var allBounds = new google.maps.LatLngBounds();
@@ -56,7 +56,7 @@ export class WorldMapComponent implements OnInit {
                 position: markerPosition,
                 label: "",
               });
-              google.maps.event.addListener(marker, 'click', function() {
+              google.maps.event.addListener(marker, 'click', function () {
                 var bounds = new google.maps.LatLngBounds(marker.getPosition());
                 this.map.fitBounds(bounds);
                 this.map.setZoom(((this.map.getZoom() - 10) > 0) ? (this.map.getZoom() - 10) : 1);
@@ -65,8 +65,8 @@ export class WorldMapComponent implements OnInit {
               return marker;
             });
             this.map.fitBounds(allBounds);
-            this.markerCluster = new MarkerClusterer(this.map, this.markers, { 
-              imagePath: "/lib/@googlemaps/markerclustererplus/m" 
+            this.markerCluster = new MarkerClusterer(this.map, this.markers, {
+              imagePath: "/lib/@googlemaps/markerclustererplus/m"
             });
           }
           google.maps.event.addListener(this.map, 'bounds_changed', (e) => {
@@ -79,28 +79,30 @@ export class WorldMapComponent implements OnInit {
           this.searchBox = new google.maps.places.SearchBox(this.gmapSearchBox.nativeElement);
           this.searchBox.addListener('places_changed', (e) => {
             var places = this.searchBox.getPlaces();
-      
-            if (places.length == 0) {
-              return;
+            if (places.length > 0) {
+              var bounds = new google.maps.LatLngBounds();
+              // For each place, get the icon, name and location.
+
+              places.forEach(function (place) {
+                if (!place.geometry) {
+                  console.log("Returned place contains no geometry");
+                  return;
+                }
+
+                if (place.geometry.viewport) {
+                  // Only geocodes have viewport.
+                  bounds.union(place.geometry.viewport);
+                } else {
+                  bounds.extend(place.geometry.location);
+                }
+              });
+
+              this.map.fitBounds(bounds);
+
+              this.viewportPhotos = this.allPhotos.filter((photo) => {
+                return bounds.contains({ lat: photo.locLat, lng: photo.locLng });
+              });              
             }
-      
-            // For each place, get the icon, name and location.
-            var bounds = new google.maps.LatLngBounds();
-            places.forEach(function (place) {
-              if (!place.geometry) {
-                console.log("Returned place contains no geometry");
-                return;
-              }
-      
-              if (place.geometry.viewport) {
-                // Only geocodes have viewport.
-                bounds.union(place.geometry.viewport);
-              } else {
-                bounds.extend(place.geometry.location);
-              }
-            });
-      
-            this.map.fitBounds(bounds);
           });
         });
     }
