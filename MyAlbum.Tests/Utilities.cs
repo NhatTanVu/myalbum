d0d46bf5b738b45654c5c89927dc1292eb1adcc8
NhatTanVu <onchua2006@gmail.com>
2020-10-25 12:43:38 +0800

    - Update Save Photo test. - Generate code coverage report.

--- MyAlbum.Tests/Utilities.cs
+++ MyAlbum.Tests/Utilities.cs
@@ -7,13 +7,15 @@ namespace MyAlbum.Tests
 {
     public class Utilities
     {
-        public static ControllerContext SetupCurrentUserForController(string expectedUserName)
+        public static ControllerContext SetupCurrentUserForController(string expectedUserName, Mock<HttpContext> mockContext = null)
         {
+            if (mockContext == null)
+                mockContext = new Mock<HttpContext>();
+
             ClaimsPrincipal claimUser = new ClaimsPrincipal();
             ClaimsIdentity claimIdentity = new ClaimsIdentity();
             claimIdentity.AddClaim(new Claim(ClaimTypes.Name, expectedUserName));
             claimUser.AddIdentity(claimIdentity);
-            var mockContext = new Mock<HttpContext>();
             mockContext.SetupGet(ctx => ctx.User).Returns(claimUser);
             ControllerContext controllerContext = new ControllerContext();
             controllerContext.HttpContext = mockContext.Object;
