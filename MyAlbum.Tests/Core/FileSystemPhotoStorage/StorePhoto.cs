cefb5791ad873d2594606940af76cea2e2543a01
NhatTanVu <onchua2006@gmail.com>
2020-11-13 20:11:22 +0800

    Add more unit tests

---
+++ MyAlbum.Tests/Core/FileSystemPhotoStorage/StorePhoto.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using MyAlbum.Core;
+using Xunit;
+
+namespace MyAlbum.Tests.Core
+{
+    public class FileSystemPhotoStorage_Test1
+    {
+        private IFormFile GetFormFile(string filePath)
+        {
+            var fileName = Path.GetFileName(filePath);
+            var ms = new MemoryStream(File.ReadAllBytes(filePath));
+            var formFile = new FormFile(ms, 0, ms.Length, filePath, filePath);
+
+            return formFile;
+        }
+
+        [Fact]
+        public async Task StorePhoto()
+        {
+            // Arrange
+            string originalFilePath = @".\car.jpg";
+            string uploadsFolderPath = @".\uploads";
+            if (!Directory.Exists(uploadsFolderPath))
+            {
+                Directory.CreateDirectory(uploadsFolderPath);
+            }
+            IFormFile file = GetFormFile(originalFilePath);
+            var filePhotoStorage = new FileSystemPhotoStorage();
+            // Act
+            var result = await filePhotoStorage.StorePhoto(file, uploadsFolderPath);
+            // Assert
+            string expectedFilePath = Path.Combine(uploadsFolderPath, result);
+            Assert.True(File.Exists(expectedFilePath));
+            File.Delete(expectedFilePath);
+        }
+    }
+}
