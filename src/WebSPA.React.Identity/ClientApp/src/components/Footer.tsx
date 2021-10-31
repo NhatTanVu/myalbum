b8314f2c6d080f81caa9408b51034bedcdfaeaac
NhatTanVu <onchua2006@gmail.com>
2021-10-31 04:06:19 -0700

    Refactor code + Update footer with API URLs from appsettings config file

--- src/WebSPA.React.Identity/ClientApp/src/components/Footer.tsx
+++ src/WebSPA.React.Identity/ClientApp/src/components/Footer.tsx
@@ -1,43 +1,66 @@
 import React, { Component } from 'react';
+import { GlobalDataService } from '../services/globalData.service';
 
-export class Footer extends Component {
+interface IFooterProps {
+}
 
-  render () {
-    return (
-      <footer id="footer">
-        <div className="container-fluid text-light background-dark-darker box-shadow-top">
-          <div className="row">
-            <div className="col-6">
-            <div className="row">
-              <div className="col-12 pt-3">
-                <h5 className="border-bottom d-inline-block">Documentation</h5>
-              </div>
-            </div>
-            <div className="row">
-              <ul className="unstyled">
-                <li><a className="nav-link text-light pt-0 pb-0" href="/swagger" target="_blank">Identity API</a></li>
-                <li><a className="nav-link text-light pt-0 pb-0" href="/swagger" target="_blank">Photo API</a></li>
-                <li><a className="nav-link text-light pt-0 pb-0" href="/swagger" target="_blank">Album API</a></li>
-                <li><a className="nav-link text-light pt-0 pb-0" href="/swagger" target="_blank">Comment API</a></li>
-              </ul>
-            </div>
-            </div>
-            <div className="col-6">
-            <div className="row">
-              <div className="col-12 pt-3">
-                <h5 className="border-bottom d-inline-block">About me</h5>
-              </div>
-            </div>
-            <div className="row">
-              <ul className="unstyled">
-                <li><a className="nav-link text-light pt-0 pb-0" href="https://github.com/NhatTanVu" target="_blank" rel="noopener noreferrer">GitHub</a></li>
-                <li><a className="nav-link text-light pt-0 pb-0" href="https://www.linkedin.com/in/tanvu/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
-              </ul>
-            </div>
-            </div>
-          </div>
-        </div>
-      </footer>      
-    );
-  }
+interface IFooterState {
+    identityUrl: string | undefined,
+    photoUrl: string | undefined,
+    albumUrl: string | undefined,
+    commentUrl: string | undefined
+}
+
+export class Footer extends Component<IFooterProps, IFooterState> {
+    private globalDataService = new GlobalDataService();
+
+    componentDidMount() {
+        this.globalDataService.getConfiguration().then(config => {
+            this.setState({
+                identityUrl: config.IssuerUri as string,
+                photoUrl: config.PhotoApiUrl?.replace("/api/photos", ""),
+                albumUrl: config.AlbumApiUrl?.replace("/api/albums", ""),
+                commentUrl: config.CommentApiUrl?.replace("/api/comments", "")
+            });
+        });
+    }
+
+    render() {
+        return (
+            <footer id="footer">
+                <div className="container-fluid text-light background-dark-darker box-shadow-top">
+                    <div className="row">
+                        <div className="col-6">
+                            <div className="row">
+                                <div className="col-12 pt-3">
+                                    <h5 className="border-bottom d-inline-block">Documentation</h5>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <ul className="unstyled">
+                                    <li><a className="nav-link text-light pt-0 pb-0" href={this.state?.identityUrl + "/swagger"} target="_blank">Identity API</a></li>
+                                    <li><a className="nav-link text-light pt-0 pb-0" href={this.state?.photoUrl + "/swagger"} target="_blank">Photo API</a></li>
+                                    <li><a className="nav-link text-light pt-0 pb-0" href={this.state?.albumUrl + "/swagger"} target="_blank">Album API</a></li>
+                                    <li><a className="nav-link text-light pt-0 pb-0" href={this.state?.commentUrl + "/swagger"} target="_blank">Comment API</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-6">
+                            <div className="row">
+                                <div className="col-12 pt-3">
+                                    <h5 className="border-bottom d-inline-block">About me</h5>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <ul className="unstyled">
+                                    <li><a className="nav-link text-light pt-0 pb-0" href="https://github.com/NhatTanVu" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                                    <li><a className="nav-link text-light pt-0 pb-0" href="https://www.linkedin.com/in/tanvu/" target="_blank" rel="noopener noreferrer">LinkedIn</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        );
+    }
 }
--- src/WebSPA.React.Identity/ClientApp/src/models/user.ts
+++ src/WebSPA.React.Identity/ClientApp/src/models/user.ts
@@ -1,19 +1,19 @@
 export class User {
-  userName: string = "";
-  firstName: string = "";
-  lastName: string = "";
-  displayName: string = "";
-  createdDate: Date = new Date();
-  modifiedDate: Date = new Date();
+    userName: string = "";
+    firstName: string = "";
+    lastName: string = "";
+    displayName: string = "";
+    createdDate: Date = new Date();
+    modifiedDate: Date = new Date();
 }
 
 export function setDisplayName(user: User) {
-  if (user.firstName && user.lastName)
-    user.displayName = user.firstName + " " + user.lastName;
-  else if (user.firstName)
-    user.displayName = user.firstName;
-  else if (user.lastName)
-    user.displayName = user.lastName;
-  else
-    user.displayName = user.userName;
+    if (user.firstName && user.lastName)
+        user.displayName = user.firstName + " " + user.lastName;
+    else if (user.firstName)
+        user.displayName = user.firstName;
+    else if (user.lastName)
+        user.displayName = user.lastName;
+    else
+        user.displayName = user.userName;
 }
